3cb72f435bd4965cb82a26f002d94873c862297a
cnwanny <cn_wanny@sina.com>
2017-07-26 18:00:20 +0800

    用工详情

---
+++ app/src/main/java/com/wanny/workease/system/workease_business/customer/work_detail/WorkDetailImpl.java
@@ -0,0 +1,13 @@
+package com.wanny.workease.system.workease_business.customer.work_detail;
+
+import com.wanny.workease.system.framework_care.OrdinalResultEntity;
+import com.wanny.workease.system.framework_mvpbasic.BaseOperateImp;
+
+/**
+ * 文件名： WorkDetailImpl
+ * 功能：
+ * 作者： wanny
+ * 时间： 14:05 2017/7/26
+ */
+public interface WorkDetailImpl extends BaseOperateImp<OrdinalResultEntity>{
+}
